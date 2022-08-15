101c58ec520d30f43b904a76ac032846f2857823
Roman <sheeproman@gmail.com>
2022-08-15 14:09:48 +0200

    code structure, cone rendering done in the ConeTrack class

--- cone_track.py
+++ cone_track.py
@@ -1,18 +1,41 @@
 import numpy as np
 from pathlib import Path
 
+from ursina import *
+
 class ConeTrack():
     def __init__(self, path):
         self.load_from_npy(path)
 
+        self.cone_model = 'cube'
+        self.cone_scale = (0.1, 0.4, 0.1)
+        self.texture = 'white_cube'
+
     def load_from_npy(self, path):
         cones = np.load(path)
 
-        self.yellow_cones = cones[cones[:,2] == 0, :2].reshape(-1,2)
-        self.blue_cones = cones[cones[:,2] == 1, :2].reshape(-1,2)
+        self.yellow_cones = cones[cones[:,2] == 1, :2].reshape(-1,2)
+        self.blue_cones = cones[cones[:,2] == 0, :2].reshape(-1,2)
         self.orange_cones = cones[cones[:,2] == 2, :2].reshape(-1,2)
         self.big_cones = cones[cones[:,2] == 3, :2].reshape(-1,2)
 
     def load_from_json(self):
         pass
+
+    def render_cones(self):
+        def render_cone(color, pos):
+            Entity(
+                model = self.cone_model,
+                color = color,
+                texture = self.texture,
+                position = pos,
+                scale = self.cone_scale
+            )
+            return
+
+        [render_cone(color.yellow, (c[0], 0, c[1])) for c in self.yellow_cones]
+        [render_cone(color.blue, (c[0], 0, c[1])) for c in self.blue_cones]
+        [render_cone(color.orange, (c[0], 0, c[1])) for c in self.orange_cones]
+        [render_cone(color.red, (c[0], 0, c[1])) for c in self.big_cones]
+        
 
--- main.py
+++ main.py
@@ -2,67 +2,54 @@ from ursina import *
 
 from cone_track import ConeTrack
 
-app = Ursina()
-
-ground = Entity(
-    model = 'cube',
-    color = color.gray,
-    # texture='brick',
-    # texture='asfalt.jpg',
-    position=(0,0,0),
-    scale = (200, 0, 200),
-    collider = 'box'
-)
-
-sky = Sky(
-    texture='sky_default',
-    color=color.cyan
-)
-
-player = Entity(
-    model = 'cube',
-    position=(1,1,1),
-    texture = 'white_cube',
-    color = color.orange,
-    scale_y = 1
-)
-
-camera.position = Vec3(0,50,-50)
-camera.rotation = (35,0,0)
-
-# cone track
-cone_track = ConeTrack('slam_hard_track.npy')
-
-for yellow_cone in cone_track.yellow_cones:
-    Entity(
+def world_setup():
+    ground = Entity(
         model = 'cube',
-        color = color.yellow,
-        texture = 'white_cube',
-        position = (yellow_cone[0], 0, yellow_cone[1]),
-        scale=(0.1,0.4,0.1)
+        color = color.gray,
+        # texture='brick',
+        # texture='asfalt.jpg',
+        position=(0,0,0),
+        scale = (200, 0, 200),
+        collider = 'box'
     )
 
-for blue_cone in cone_track.blue_cones:
-    Entity(
-        model = 'cube',
-        color = color.blue,
-        texture = 'white_cube',
-        position = (blue_cone[0], 0, blue_cone[1]),
-        scale=(0.1,0.4,0.1)
+    sky = Sky(
+        texture='sky_default',
+        color=color.cyan
     )
 
+if __name__ == '__main__':
+
+    app = Ursina()
+
+    # 1. SETUP WORLD
+    world_setup()
+
+    # 2. RENDER CONES
+    cone_track = ConeTrack('slam_hard_track.npy')
+    cone_track.render_cones()
+
+    # 3. RENDER THE CAR
+    player = Entity(
+        model = 'sphere',
+        position=(1,1,1),
+        texture = 'white_cube',
+        color = color.orange,
+        scale_y = 1
+    )
+
+    camera.position = Vec3(0,50,-50)
+    camera.rotation = (35,0,0)
 
 
-# Sky(color = color.blue)
+    print(camera.position)
 
-print(camera.position)
+    def update():                  # update gets automatically called by the engine.
+        player.x += held_keys['d'] * .1
+        player.x -= held_keys['a'] * .1
+        player.z += held_keys['w'] * .1
+        player.z -= held_keys['s'] * .1
 
-def update():                  # update gets automatically called by the engine.
-    player.x += held_keys['d'] * .1
-    player.x -= held_keys['a'] * .1
-    player.z += held_keys['w'] * .1
-    player.z -= held_keys['s'] * .1
+        camera.rotation
 
-    camera.rotation
-
-app.run()
+    app.run()
