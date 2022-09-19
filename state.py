98f31360847ff876ac0a16356720369e38afb8cc
SnakeOnex <sheeproman@gmail.com>
2022-09-19 21:40:20 +0200

    car controls in separate function now

--- state.py
+++ state.py
@@ -26,7 +26,37 @@ class State():
 
     def update_state(self, timedelta):
 
-        ## CONTROLS
+        # CONTROLS
+        self.handle_controls()
+        
+        # PHYSICS
+        
+        ## LONGITUDINAL
+        F_traction = self.engine_force # tractive force
+        F_drag = -self.drag_coef * self.speed**2 # air resistance
+        F_rr = -self.rr_coef * self.speed # rolling resistance
+        F_long = F_traction + F_drag + F_rr # longitudinal force
+        self.car_pos -= self.rotation_vector
+        acc = F_long / self.mass # acceleration
+        self.speed += acc * timedelta 
+
+        ## LATERAL
+        turn_radius = self.wheel_base / np.sin(np.deg2rad(self.steering_angle))
+        rotation = self.speed / (turn_radius*3.6)
+        self.heading += timedelta * np.rad2deg(rotation)
+        self.rotation_vector = rotate_around_point(self.heading,(0,0),(-self.wheel_base,0))
+
+        if self.heading >= 360.:
+            self.heading -= 360
+        if self.heading < 0.:
+            self.heading += 360
+
+        heading_vec = angle_to_vector(self.heading)
+        velocity = heading_vec * self.speed
+        self.car_pos += self.rotation_vector
+        self.car_pos += timedelta * velocity
+
+    def handle_controls(self):
         if self.steering_control == "LEFT":
             self.steering_angle += timedelta * self.steering_speed
 
@@ -62,31 +92,6 @@ class State():
             self.traction_control = "NEUTRAL"
         elif self.traction_control == "NEUTRAL":
             self.engine_force = 0.
-
-        # LONGITUDINAL
-        F_traction = self.engine_force # tractive force
-        F_drag = -self.drag_coef * self.speed**2 # air resistance
-        F_rr = -self.rr_coef * self.speed # rolling resistance
-        F_long = F_traction + F_drag + F_rr # longitudinal force
-        self.car_pos -= self.rotation_vector
-        acc = F_long / self.mass # acceleration
-        self.speed += acc * timedelta 
-
-        # LATERAL
-        turn_radius = self.wheel_base / np.sin(np.deg2rad(self.steering_angle))
-        rotation = self.speed / (turn_radius*3.6)
-        self.heading += timedelta * np.rad2deg(rotation)
-        self.rotation_vector = rotate_around_point(self.heading,(0,0),(-self.wheel_base,0))
-
-        if self.heading >= 360.:
-            self.heading -= 360
-        if self.heading < 0.:
-            self.heading += 360
-
-        heading_vec = angle_to_vector(self.heading)
-        velocity = heading_vec * self.speed
-        self.car_pos += self.rotation_vector
-        self.car_pos += timedelta * velocity
 
     def load_map(self, map_filepath):
         with open(map_filepath, 'r') as f:
