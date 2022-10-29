7a20e593e000e609cf6dc76acbaba24171c3f69f
Roman <sheeproman@gmail.com>
2022-10-30 00:48:07 +0200

    added some time functionality to the test

--- tests/test_acceleration.py
+++ tests/test_acceleration.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import time
+import multiprocessing
 from multiprocessing import Process
 
 from dv_sim.sim.simulation import Simulation
@@ -7,7 +8,7 @@ from master import main
 
 def test_acceleration():
     # 1. SETUP
-    def run_sim():
+    def run_sim(ret):
         map_path = Path("dv_sim/maps/acceleration_map.json").resolve()
         print("map_path: ", map_path)
         sim = Simulation(
@@ -19,17 +20,24 @@ def test_acceleration():
 
         start_time = time.perf_counter()
 
+        # simulation loop
         while True:
             sim.step()
-
             time_since_start = time.perf_counter() - start_time
 
-            if time_since_start >= 5.:
+            # after 10 seconds send go signal
+            if time_since_start >= 10.:
                 sim.go_signal()
+
+            # after 20 seconds end and return TRUE
+            if time_since_start >= 30.:
+                ret.value = True
+                break
 
             sim.sleep()
 
-    sim_process = Process(target=run_sim)
+    ret = multiprocessing.Value('b', False)
+    sim_process = Process(target=run_sim, args=(ret,))
     sim_process.start()
 
     bros_process = Process(target=main)
@@ -37,7 +45,7 @@ def test_acceleration():
 
     sim_process.join()
 
-    assert True
+    assert ret.value
 
 if __name__ == '__main__':
     test_acceleration()
