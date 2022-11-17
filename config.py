6cee5ac35ccc562fd10e6018d712f354ce1d38ab
edavidk7 <david.korcak@gmail.com>
2022-11-17 13:17:28 +0100

    Moved simulation config from hardcoded values to config.py

--- config.py
+++ config.py
@@ -47,6 +47,20 @@ vision_node_config = {
     "logging_opt": logging_opt
 }
 
+tcp_config = {
+    "TCP_HOST": '127.0.0.1',
+    "VISION_PORT": '50000',
+    "GUI_PORT": '50001',
+    "CONTROLS_PORT": '50002',
+    "AS_DEBUG_PORT": '50003'
+}
+
+can_config = {
+    "CAN1_ID": 0,
+    "CAN2_ID": 1,
+    "CAN_JSON": "data/D1.json"
+}
+
 ##########################
 # TRACKDRIVE NODE CONFIG #
 ##########################
