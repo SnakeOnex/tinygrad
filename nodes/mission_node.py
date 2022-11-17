8267119c450e4f6a23b28322ad3faa63870cd43e
edavidk7 <david.korcak@gmail.com>
2022-11-17 15:24:55 +0100

    Added loading parameters from confing in mission_node.py

--- nodes/mission_node.py
+++ nodes/mission_node.py
@@ -10,8 +10,10 @@ from nodes.can2_node import Can2RecvItems, Can2SendItems
 
 from missions.trackdrive import Trackdrive
 from nodes.asm import ASM, AS
+from config import can_config
 
 from pycandb.can_interface import CanInterface
+
 
 class MissionNode(mp.Process):
     def __init__(self, perception_out, can1_recv_name, can2_recv_name):
@@ -20,17 +22,20 @@ class MissionNode(mp.Process):
         self.can1_recv_name = can1_recv_name
         self.can2_recv_name = can2_recv_name
 
-        self.frequency = 10 # Hz
+        self.frequency = 10  # Hz
 
         # Autonomous State Machine
         self.ASM = ASM()
 
     def initialize(self):
-        self.can1_recv_state = shared_memory.ShareableList(name=self.can1_recv_name)
-        self.can2_recv_state = shared_memory.ShareableList(name=self.can2_recv_name)
+        self.can1_recv_state = shared_memory.ShareableList(
+            name=self.can1_recv_name)
+        self.can2_recv_state = shared_memory.ShareableList(
+            name=self.can2_recv_name)
         self.mission = Trackdrive(self.perception_out, self.can1_recv_state)
 
-        self.CAN1 = CanInterface("data/D1.json", 0, False)
+        self.CAN1 = CanInterface(
+            can_config["CAN_JSON"], can_config["CAN1_ID"], False)
 
     def run(self):
         self.initialize()
@@ -39,19 +44,22 @@ class MissionNode(mp.Process):
             start_time = time.perf_counter()
 
             # 1. update AS State
-            self.ASM.update(go_signal=self.can2_recv_state[Can2RecvItems.go_signal.value])
+            self.ASM.update(
+                go_signal=self.can2_recv_state[Can2RecvItems.go_signal.value])
 
             print("AS: ", self.ASM.AS)
 
             if self.ASM.AS == AS.DRIVING:
                 steering_angle, speed = self.mission.loop()
 
-                self.CAN1.send_can_msg([steering_angle], self.CAN1.name2id["XVR_Control"])
-                self.CAN1.send_can_msg([0, 0, 0, 0, speed, 0], self.CAN1.name2id["XVR_SetpointsMotor_A"])
+                self.CAN1.send_can_msg(
+                    [steering_angle], self.CAN1.name2id["XVR_Control"])
+                self.CAN1.send_can_msg(
+                    [0, 0, 0, 0, speed, 0], self.CAN1.name2id["XVR_SetpointsMotor_A"])
 
-            
             # 3. send XVR_STATUS
-            self.CAN1.send_can_msg([self.ASM.AS.value, 0, 0, 0, 0, 0, 0, 0], self.CAN1.name2id["XVR_Status"])
+            self.CAN1.send_can_msg(
+                [self.ASM.AS.value, 0, 0, 0, 0, 0, 0, 0], self.CAN1.name2id["XVR_Status"])
             # print("sent XVR_STATUS")
 
             end_time = time.perf_counter()
