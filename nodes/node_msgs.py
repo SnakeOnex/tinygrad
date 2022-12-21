37a7291ad83cfb091cf55b170469fd8bcc8c9ad5
Vooba27 <voborja5@fel.cvut.cz>
2022-12-21 12:59:55 +0100

    Added interface for updating subscription data. Module now fully encapsulates pickle serizalization

--- nodes/node_msgs.py
+++ nodes/node_msgs.py
@@ -1,4 +1,5 @@
 import zmq
+import pickle
 from enum import IntEnum, auto
 
 from config import tcp_config
@@ -16,10 +17,24 @@ def create_subscriber_socket(port):
     socket.setsockopt(zmq.SUBSCRIBE, b"")
     return socket
 
+def publish_data(socket, data):
+    socket.send(pickle.dumps(data))
+
 def get_last_subscription_data(socket):
     data = None
     while socket.poll(0.):
         data = socket.recv()
+
+    if data is not None:
+        data = pickle.loads(data)
+
+    return data
+
+def update_subscription_data(socket, old_data):
+    data = get_last_subscription_data(socket)
+    if data is None:
+        data = old_data
+    
     return data
 
 class NodePorts(IntEnum):
