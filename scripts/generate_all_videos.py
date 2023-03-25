0fa08f59f6ea657c8e9e3b7373baa585ae177106
Roman <sheeproman@gmail.com>
2023-03-25 11:50:13 +0100

    script for video generation doesn't generate a video if it already exists

--- scripts/generate_all_videos.py
+++ scripts/generate_all_videos.py
@@ -12,4 +12,9 @@ if __name__ == '__main__':
 
     for log_folder in bros_data_path.iterdir():
         AS_folder = Path(log_folder) / Path("AS")
-        log_to_video(AS_folder)
+
+        if ".mp4" not in [file.suffix for file in AS_folder.iterdir()]:
+            log_to_video(AS_folder, SKIP=3)
+            print(f"processing {log_folder}")
+        else:
+            print(f"skipping {log_folder}")
