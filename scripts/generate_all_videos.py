4e8f7daa3342ffee5edcce1de4987915cda7cf04
Roman <sheeproman@gmail.com>
2023-04-06 15:13:50 +0200

    removed SKIP argument from video generation scripo

--- scripts/generate_all_videos.py
+++ scripts/generate_all_videos.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         AS_folder = Path(log_folder) / Path("AS")
 
         if ".mp4" not in [file.suffix for file in AS_folder.iterdir()]:
-            log_to_video(AS_folder, SKIP=3)
+            log_to_video(AS_folder)
             print(f"processing {log_folder}")
         else:
             print(f"skipping {log_folder}")
