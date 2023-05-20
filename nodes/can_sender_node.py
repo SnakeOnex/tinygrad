e561d64199543d12d5052496f165aefea953b187
Roman <sheeproman@gmail.com>
2023-05-20 17:05:45 +0200

    fixed torques in cansender

--- nodes/can_sender_node.py
+++ nodes/can_sender_node.py
@@ -20,12 +20,13 @@ class CanSenderNode(mp.Process):
         self.CAN2 = CanInterface(can_config["CAN_JSON"], can_config["CAN2_ID"], recv_self=False, log_data_dir=curr_log_folder, log_messages=True)
         self.loop_counter = 0
         self.wheel_speed_cmd = 0.
+        self.torque_cmd = 0.
         self.steering_angle_cmd = 0.
         self.ksicht_status = (0, 0, 0, 0, 0, 0, 0, 0)
 
         # CAN msgs values
-        power = 5 # Nm
-        self.ksicht_status_values = [self.ksicht_status[0], self.ksicht_status[1], 0, 0, 0, 0, 0, 5]
+        
+        self.ksicht_status_values = [self.ksicht_status[0], self.ksicht_status[1], 0, 0, 0, 0, 0, 0]
         self.motor_setpoints_values = [0, 0, 0, 0, self.wheel_speed_cmd, 0]
         self.steering_control_values = [self.steering_angle_cmd]
 
@@ -43,7 +44,7 @@ class CanSenderNode(mp.Process):
         self.init_res()
 
     def update_data(self):
-        self.wheel_speed_cmd = update_subscription_data(self.wheel_speed_cmd_socket, self.wheel_speed_cmd)
+        self.wheel_speed_cmd, self.torque_cmd = update_subscription_data(self.wheel_speed_cmd_socket, [self.wheel_speed_cmd,self.torque_cmd])
         self.steering_angle_cmd = update_subscription_data(self.steering_angle_cmd_socket, self.steering_angle_cmd)
         self.ksicht_status = update_subscription_data(self.ksicht_status_socket, self.ksicht_status)
 
@@ -51,6 +52,7 @@ class CanSenderNode(mp.Process):
         self.ksicht_status_values = list(self.ksicht_status)
 
         self.motor_setpoints_values[4] = self.wheel_speed_cmd
+        self.motor_setpoints_values[5] = self.torque_cmd
         self.steering_control_values[0] = self.steering_angle_cmd
 
     def run(self):
