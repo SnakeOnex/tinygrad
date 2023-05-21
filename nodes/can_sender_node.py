4d461418117fab5df783c18cdc947316b05b76de
Roman <sheeproman@gmail.com>
2023-05-21 10:06:31 +0200

    can sender car status 2

--- nodes/can_sender_node.py
+++ nodes/can_sender_node.py
@@ -9,7 +9,7 @@ from pycandb.can_interface import CanInterface
 from config import can_config, tcp_config
 from config import MissionNodeMsgPorts
 from config import can_sender_config as opt
-from config import AS
+from config import AS, CarStatus, CAN1NodeMsgPorts
 from config import MissionValue
 class CanSenderNode(mp.Process):
     def __init__(self, curr_log_folder):
@@ -72,7 +72,7 @@ class CanSenderNode(mp.Process):
                 self.logger.log("XVR_Status", self.ksicht_status_values)
 
                 # Lenze inverters require extended CAN address ID
-                if self.ksicht_status_values[0] == AS.DRIVING or self.ksicht_status_values[1] == MissionValue.Manual:
+                if self.ksicht_status_values[0] == AS.DRIVING or (self.ksicht_status_values[1] == MissionValue.Manual and self.car_status == CarStatus.TS_STARTED):
                     self.CAN1.send_can_msg([1,0,0,0],self.CAN1.name2id["XVR_MasterControlStatus"], is_extended_id=True)
                     self.motor_setpoints_values[0] = 1
                 else:
